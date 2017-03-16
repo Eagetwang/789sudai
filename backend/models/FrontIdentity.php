025726e788a92c6ab1a51f61ecec2b7288c99dd9
Eagetwang <1210975234@qq.com>
2017-03-16 12:56:57 +0800

    身份类别表

---
+++ backend/models/FrontIdentity.php
@@ -0,0 +1,213 @@
+<?php
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "front_identity".
+ *
+ * @property string $id
+ * @property string $name
+ * @property string $update_date
+ * @property string $create_date
+ * @property string $update_user
+ * @property string $create_user
+ */
+class FrontIdentity extends \backend\models\BaseModel
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'front_identity';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'update_date', 'create_date', 'create_user'], 'required'],
+            [['update_date', 'create_date'], 'safe'],
+            [['name'], 'string', 'max' => 50],
+            [['update_user', 'create_user'], 'string', 'max' => 20]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => '身份名称',
+            'update_date' => '更新时间',
+            'create_date' => '创建时间',
+            'update_user' => '更新人',
+            'create_user' => '创建人',
+        ];
+    }
+
+  /**
+     * 返回数据库字段信息，仅在生成CRUD时使用，如不需要生成CRUD，请注释或删除该getTableColumnInfo()代码
+     * COLUMN_COMMENT可用key如下:
+     * label - 显示的label
+     * inputType 控件类型, 暂时只支持text,hidden  // select,checkbox,radio,file,password,
+     * isEdit   是否允许编辑，如果允许编辑将在添加和修改时输入
+     * isSearch 是否允许搜索
+     * isDisplay 是否在列表中显示
+     * isOrder 是否排序
+     * udc - udc code，inputtype为select,checkbox,radio三个值时用到。
+     * 特别字段：
+     * id：主键。必须含有主键，统一都是id
+     * create_date: 创建时间。生成的代码自动赋值
+     * update_date: 修改时间。生成的代码自动赋值
+     */
+    public function getTableColumnInfo(){
+        return array(
+        'id' => array(
+                        'name' => 'id',
+                        'allowNull' => false,
+//                         'autoIncrement' => true,
+//                         'comment' => '',
+//                         'dbType' => "int(11) unsigned",
+                        'defaultValue' => '',
+                        'enumValues' => null,
+                        'isPrimaryKey' => true,
+                        'phpType' => 'string',
+                        'precision' => '11',
+                        'scale' => '',
+                        'size' => '11',
+                        'type' => 'integer',
+                        'unsigned' => true,
+                        'label'=>$this->getAttributeLabel('id'),
+                        'inputType' => 'hidden',
+                        'isEdit' => true,
+                        'isSearch' => true,
+                        'isDisplay' => true,
+                        'isSort' => true,
+//                         'udc'=>'',
+                    ),
+		'name' => array(
+                        'name' => 'name',
+                        'allowNull' => false,
+//                         'autoIncrement' => false,
+//                         'comment' => '身份名称',
+//                         'dbType' => "varchar(50)",
+                        'defaultValue' => '',
+                        'enumValues' => null,
+                        'isPrimaryKey' => false,
+                        'phpType' => 'string',
+                        'precision' => '50',
+                        'scale' => '',
+                        'size' => '50',
+                        'type' => 'string',
+                        'unsigned' => false,
+                        'label'=>$this->getAttributeLabel('name'),
+                        'inputType' => 'text',
+                        'isEdit' => true,
+                        'isSearch' => true,
+                        'isDisplay' => true,
+                        'isSort' => false,
+//                         'udc'=>'',
+                    ),
+		'update_date' => array(
+                        'name' => 'update_date',
+                        'allowNull' => false,
+//                         'autoIncrement' => false,
+//                         'comment' => '更新时间',
+//                         'dbType' => "datetime",
+                        'defaultValue' => '',
+                        'enumValues' => null,
+                        'isPrimaryKey' => false,
+                        'phpType' => 'string',
+                        'precision' => '',
+                        'scale' => '',
+                        'size' => '',
+                        'type' => 'datetime',
+                        'unsigned' => false,
+                        'label'=>$this->getAttributeLabel('update_date'),
+                        'inputType' => 'text',
+                        'isEdit' => false,
+                        'isSearch' => false,
+                        'isDisplay' => false,
+                        'isSort' => false,
+//                         'udc'=>'',
+                    ),
+		'create_date' => array(
+                        'name' => 'create_date',
+                        'allowNull' => false,
+//                         'autoIncrement' => false,
+//                         'comment' => '创建时间',
+//                         'dbType' => "datetime",
+                        'defaultValue' => '',
+                        'enumValues' => null,
+                        'isPrimaryKey' => false,
+                        'phpType' => 'string',
+                        'precision' => '',
+                        'scale' => '',
+                        'size' => '',
+                        'type' => 'datetime',
+                        'unsigned' => false,
+                        'label'=>$this->getAttributeLabel('create_date'),
+                        'inputType' => 'text',
+                        'isEdit' => false,
+                        'isSearch' => false,
+                        'isDisplay' => false,
+                        'isSort' => false,
+//                         'udc'=>'',
+                    ),
+		'update_user' => array(
+                        'name' => 'update_user',
+                        'allowNull' => true,
+//                         'autoIncrement' => false,
+//                         'comment' => '更新人',
+//                         'dbType' => "varchar(20)",
+                        'defaultValue' => '',
+                        'enumValues' => null,
+                        'isPrimaryKey' => false,
+                        'phpType' => 'string',
+                        'precision' => '20',
+                        'scale' => '',
+                        'size' => '20',
+                        'type' => 'string',
+                        'unsigned' => false,
+                        'label'=>$this->getAttributeLabel('update_user'),
+                        'inputType' => 'text',
+                        'isEdit' => false,
+                        'isSearch' => false,
+                        'isDisplay' => false,
+                        'isSort' => false,
+//                         'udc'=>'',
+                    ),
+		'create_user' => array(
+                        'name' => 'create_user',
+                        'allowNull' => false,
+//                         'autoIncrement' => false,
+//                         'comment' => '创建人',
+//                         'dbType' => "varchar(20)",
+                        'defaultValue' => '',
+                        'enumValues' => null,
+                        'isPrimaryKey' => false,
+                        'phpType' => 'string',
+                        'precision' => '20',
+                        'scale' => '',
+                        'size' => '20',
+                        'type' => 'string',
+                        'unsigned' => false,
+                        'label'=>$this->getAttributeLabel('create_user'),
+                        'inputType' => 'text',
+                        'isEdit' => false,
+                        'isSearch' => false,
+                        'isDisplay' => false,
+                        'isSort' => false,
+//                         'udc'=>'',
+                    ),
+		        );
+        
+    }
+ 
+}
